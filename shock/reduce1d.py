e1c1d20bc629f270cb4e97e66f80a7142baa8ee3
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2025-07-13 11:07:50 +0900

    Added plot functionality

--- shock/reduce1d.py
+++ shock/reduce1d.py
@@ -10,13 +10,12 @@ import msgpack
 import toml
 import json
 import tqdm
-
 import numpy as np
 import scipy.ndimage as ndimage
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 mpl.use("Agg") if __name__ == "__main__" else None
-import matplotlib.pyplot as plt
 
 # global configuration
 plt.rcParams.update({"font.size": 12})
@@ -63,14 +62,16 @@ class JobExecutor:
             parameter = obj["configuration"]["parameter"]
         return parameter
 
-    def get_filename(self, prefix, ext):
+    def get_dirname(self):
         dirname = self.options.get("dirname", None)
         if dirname is None:
             raise ValueError("dirname is not specified")
         elif not os.path.exists(dirname):
             os.makedirs(dirname)
+        return dirname
 
-        return os.sep.join([dirname, prefix + ext])
+    def get_filename(self, prefix, ext):
+        return os.sep.join([self.get_dirname(), prefix + ext])
 
     def main(self, prefix):
         raise NotImplementedError
@@ -251,6 +252,182 @@ class DataReducer(JobExecutor):
                     fp["P" + name][i, ..., 5] = Pzx
 
 
+class DataPlotter(JobExecutor):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if "plot" in kwargs:
+            for key in kwargs["plot"]:
+                self.options[key] = kwargs["plot"][key]
+
+    def get_shock_poly_fit(self, filename, fit_steps=None):
+        with h5py.File(filename, "r") as fp:
+            config = pickle.loads(fp["config"][()])
+            x = fp["x"][()]
+            B = fp["B"][()]
+            t_arr = fp["t"][()]
+            if fit_steps is None:
+                fit_steps = np.arange(0, t_arr.size)
+            params = config["parameter"]
+            bx = B[..., 0]
+            by = B[..., 1]
+            bz = B[..., 2]
+            bb = np.sqrt(bx**2 + by**2 + bz**2)
+            t_sh, x_sh, v_sh, poly_sh = utils.calc_shock_speed(
+                params, fit_steps, t_arr, x, bb, 0.01
+            )
+        return poly_sh
+
+    def convert_to_momentum_spectrum(self, energy, dist):
+        p = np.sqrt((energy + 1) ** 2 - 1)
+        e2p = 1 / (4 * np.pi * p * np.sqrt(p**2 + 1))
+        if dist.ndim == 1 and dist.size == p.size:
+            return p, dist * e2p
+        elif dist.ndim == 2 and dist.shape[1] == p.size:
+            return p, dist * e2p[np.newaxis, :]
+        else:
+            raise ValueError("Invalid shape of distribution function.")
+
+    def pcolormesh_args(self, xedges, yedges):
+        x = 0.5 * (xedges[+1:] + xedges[:-1])
+        y = 0.5 * (yedges[+1:] + yedges[:-1])
+        X, Y = np.broadcast_arrays(x[:, None], y[None, :])
+        return X, Y
+
+    def plot(self, idx, poly_sh, png, params):
+        # get only step-dependent data
+        x = params["x"]
+        B = params["B"]
+        Vi = params["Vi"]
+        fuy = params["fuy"]
+        xbinc = params["xbinc"]
+        xbine = params["xbine"]
+        ubine = params["ubine"]
+        ebine = params["ebine"]
+        step_arr = params["step_arr"]
+        t_arr = params["t_arr"]
+
+        mime = params["mime"]
+        sigma = params["sigma"]
+        vae = np.sqrt(sigma)
+        vai = vae / np.sqrt(mime)
+        wci = np.sqrt(sigma) / mime
+        u0 = params["u0"]
+        b0 = np.sqrt(sigma) / np.sqrt(1 + u0**2)
+        x_shock = np.polyval(poly_sh, t_arr[idx])
+
+        # get per-step energy distribution and momentum spectrum
+        f_ene_step = params["f_ene"][idx]
+        pbinc, f_mom = self.convert_to_momentum_spectrum(params["ebinc"], f_ene_step)
+        pbine = np.sqrt((ebine + 1) ** 2 - 1)
+
+        fig, axs = plt.subplots(5, 1, figsize=(8, 12), sharex=True)
+        plt.sca(axs[0])
+        plt.plot(x, B[idx, :, 0] / b0, "k-", label="Bx")
+        plt.plot(x, B[idx, :, 1] / b0, "r-", label="By")
+        plt.plot(x, B[idx, :, 2] / b0, "b-", label="Bz")
+        plt.ylabel(r"$B / B_0$")
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+
+        plt.sca(axs[1])
+        plt.plot(x, Vi[idx, :, 0] / vai, "k-", label="Vx")
+        plt.plot(x, Vi[idx, :, 1] / vai, "r-", label="Vy")
+        plt.plot(x, Vi[idx, :, 2] / vai, "b-", label="Vz")
+        plt.ylabel(r"$V / V_{A,i}$")
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+
+        plt.sca(axs[2])
+        X, Y = self.pcolormesh_args(xbine, ubine)
+        plt.pcolormesh(X, Y, fuy[idx], shading="nearest", norm=mpl.colors.LogNorm())
+        plt.ylabel(r"$u_y$")
+
+        plt.sca(axs[3])
+        X, Y = self.pcolormesh_args(xbine, pbine)
+        plt.pcolormesh(
+            X,
+            Y,
+            f_mom * Y**4,
+            shading="nearest",
+            norm=mpl.colors.LogNorm(),
+        )
+        plt.ylabel(r"$p^4 f(p)$")
+        plt.semilogy()
+
+        plt.sca(axs[4])
+        psample = [0.6, 0.7, 0.8, 0.9, 1.0]
+        pindex = np.searchsorted(pbine, psample)
+        for i in range(len(psample)):
+            f_norm = f_mom[:, pindex[i]] / np.max(f_mom[:, pindex[i]])
+            plt.plot(
+                xbinc,
+                f_norm * 10 ** (-i),
+                label=r"$p/m_e c = {:.1f}$".format(psample[i]),
+            )
+        plt.ylabel(r"$f(p)$ [arb. unit]")
+        plt.semilogy()
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+        plt.ylim(1.0e-6, 1.0e1)
+        plt.xlim(max(x_shock - 200, 0), x_shock + 200)
+        for ax in axs:
+            ax.grid(True)
+        fig.align_ylabels(axs)
+        step = step_arr[idx]
+        time = t_arr[idx]
+        plt.suptitle(r"$\Omega_{{ci}} t$ = {:.2f}".format(wci * time))
+        plt.tight_layout()
+        plt.savefig(png)
+        plt.close(fig)
+
+    def main(self, prefix):
+        filename = self.get_filename(prefix, ".h5")
+        output = self.options.get("output", "plot")
+        png = os.sep.join([self.get_dirname(), output])
+
+        poly_sh = self.get_shock_poly_fit(filename)
+        print("Shock speed polynomial fit:", poly_sh)
+
+        with h5py.File(filename, "r") as fp:
+            # read common parameters once
+            config = pickle.loads(fp["config"][()])
+            xbine = fp["xbine"][()]
+            ubine = fp["ubine"][()]
+            ebine = fp["ebine"][()]
+            x = fp["x"][()]
+            B = fp["B"][()]
+            Vi = fp["Vi"][()]
+            fuy = fp["Feu"][..., 1]
+            f_ene = fp["Feu"][..., 3]
+            xbinc = 0.5 * (xbine[1:] + xbine[:-1])
+            ebinc = 0.5 * (ebine[1:] + ebine[:-1])
+            step_arr = fp["step"][()]
+            t_arr = fp["t"][()]
+            params = {
+                "config": config,
+                "xbine": xbine,
+                "ubine": ubine,
+                "ebine": ebine,
+                "ebinc": ebinc,
+                "x": x,
+                "B": B,
+                "Vi": Vi,
+                "fuy": fuy,
+                "f_ene": f_ene,
+                "xbinc": xbinc,
+                "step_arr": step_arr,
+                "t_arr": t_arr,
+                "mime": config["parameter"]["mime"],
+                "sigma": config["parameter"]["sigma"],
+                "u0": config["parameter"]["u0"],
+            }
+            # make plots
+            for i in range(B.shape[0]):
+                self.plot(i, poly_sh, png + "-{:08d}.png".format(i), params)
+                print("Plot saved to:", png + "-{:08d}.png".format(i))
+
+        # convert to mp4
+        fps = self.options.get("fps", 10)
+        picnix.convert_to_mp4("{:s}".format(png), fps, False)
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -292,4 +469,7 @@ if __name__ == "__main__":
     # perform the job
     if args.job == "reduce":
         obj = DataReducer(**config)
+        obj.main(args.prefix)
+    elif args.job == "plot":
+        obj = DataPlotter(**config)
         obj.main(args.prefix)
